7caa15c3b7832e771bbbbc96078f2203a7b8985f
Rafa Zahra <rafazahra2929@gmail.com>
2020-12-10 15:58:27 +0700

    rest API

---
+++ src/pages/Rendering.vue
@@ -0,0 +1,16 @@
+<template>
+    <main-layout>
+        <h1>RENDERING</h1>
+    </main-layout>
+</template>    
+ 
+    <script>
+        import MainLayout from '../layouts/Main.vue'
+
+        export default {
+            components: {
+                MainLayout
+            }
+        }
+                
+    </script>
